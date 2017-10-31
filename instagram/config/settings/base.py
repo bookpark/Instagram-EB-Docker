dd6801c6f0626edc8349ecc3e7333b49ab1f9214
Booki Park <bkbkgg@gmail.com>
2017-10-31 11:56:30 +0900

    AWS 설정 dev로 이동

--- instagram/config/settings/base.py
+++ instagram/config/settings/base.py
@@ -33,21 +33,8 @@ STATICFILES_DIRS = [
 ]
 
 # Media paths
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# AWS
-AWS_ACCESS_KEY_ID = config_secret_common['aws']['access_key_id']
-AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['secret_access_key']
-AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['s3_bucket_name']
-
-# AWS Storage
-STATICFILES_LOCATION = 'static'
-MEDIAFILES_LOCATION = 'media'
-
-# S3 FileStorage
-DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 # Others
 WSGI_APPLICATION = 'config.wsgi.application'
