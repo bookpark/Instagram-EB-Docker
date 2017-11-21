5c913f8151c97f408fe83f09a424f782a04c8a37
Booki Park <bkbkgg@gmail.com>
2017-11-21 16:30:10 +0900

    deploy 세팅 및 logger, handler 설정

--- instagram/config/settings/local.py
+++ instagram/config/settings/local.py
@@ -1,11 +1,16 @@
+import logging
 import random
 import string
 
 from .base import *
 
+logger = logging.getLogger(__name__)
+
+DEBUG = False
+
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
+    # 'localhost',
+    # '127.0.0.1',
     '.elasticbeanstalk.com',
     '.booki.kr',
 ]
@@ -18,3 +23,27 @@ DATABASES = {
 }
 
 SECRET_KEY = ''.join([random.choice(string.ascii_lowercase) for i in range(40)])
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(ROOT_DIR, '.log', 'django.log')
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': [
+                'console',
+                'file',
+            ],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
